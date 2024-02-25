2a522ede1df6e51d06fffe91264832744545d45d
Lishwar K <jayaklish@gmail.com>
2024-02-25 23:05:07 +0530

    commiting

---
+++ Algorithms/src/unionFind/PathCompressionQuickUnion.java
@@ -0,0 +1,43 @@
+package unionFind;
+
+public class PathCompressionQuickUnion {
+    private int[] parent;
+
+    public PathCompressionQuickUnion(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    private int find(int p) {
+        int root = p;
+        while (parent[root] != root) {
+            parent[root] = parent[parent[root]]; // Path compression
+            root = parent[root];
+        }
+        return root;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP != rootQ) {
+            parent[rootP] = rootQ;
+        }
+    }
+
+    public static void main(String[] args) {
+        PathCompressionQuickUnion uf = new PathCompressionQuickUnion(10);
+        uf.union(0, 1);
+        uf.union(1, 2);
+        uf.union(2, 3);
+
+        System.out.println(uf.connected(0, 3)); // Should print true
+        System.out.println(uf.connected(4, 5)); // Should print false
+    }
+}
