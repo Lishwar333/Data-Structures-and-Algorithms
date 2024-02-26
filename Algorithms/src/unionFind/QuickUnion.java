d9782548c2eb345b537442adfcbcb1c86389cea0
Lishwar K <jayaklish@gmail.com>
2024-02-26 23:43:25 +0530

    committing

--- Algorithms/src/unionFind/QuickUnion.java
+++ Algorithms/src/unionFind/QuickUnion.java
@@ -12,37 +12,36 @@ public class QuickUnion {
 
     // Union operation - connect two elements by setting one root as the parent of the other.
     public void union(int p, int q) {
-        int rootP = findRoot(p);
-        int rootQ = findRoot(q);
-
-        if (rootP != rootQ) {
-            parent[rootP] = rootQ;  // Make rootQ the parent of rootP.
-        }
+        
+    	int root1 = findRoot(p);
+    	int root2 = findRoot(q);
+    	
+    	if(root1 != root2) { 
+    		parent[p]=root2; // Make rootQ the parent of rootP.
+    	}
+    	else {
+    		System.out.println("They have same roots/ already connected");
+    	}
     }
 
     // Find operation - find the root of the element's tree (set).
     public int findRoot(int p) {
         while (p != parent[p]) {
-            p = parent[p];  // Traverse up the tree to find the root.
+        	p = parent[p]; // Traverse up the tree to find the root.
         }
-        return p;
+     return p;   
     }
 
     public static void main(String[] args) {
         int n = 10;
         QuickUnion quickUnion = new QuickUnion(n);
-
-        // Creating a worst-case scenario where the tree becomes a linked list.
-        for (int i = 0; i < n - 1; i++) {
-            quickUnion.union(i, i + 1);
-        }
-
-        // Perform a find operation on the last element.
-        int root = quickUnion.findRoot(n - 1);
-
-        // The tree is a linked list, and the height is equal to the number of elements.
-        // Both union and find operations have O(N) time complexity in this worst-case scenario.
         
-        System.out.print(root);
+        
+        quickUnion.union(0, 6);
+        quickUnion.union(1, 6);
+        quickUnion.union(4, 5);
+        quickUnion.union(0, 6);
+        
+        System.out.print(quickUnion.findRoot(1));
     }
 }
