5703d9604d236131081a2ac822d89c0dc450cf18
Lishwar K <jayaklish@gmail.com>
2024-02-29 02:01:34 +0530

    commiting

---
+++ Algorithms/src/unionFind/Precolation.java
@@ -0,0 +1,14 @@
+package unionFind;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Precolation {
+
+	public static void main(String[] args) {
+		
+		WeightedQuickUnionUF wuf = new WeightedQuickUnionUF(10);
+	}
+
+}
